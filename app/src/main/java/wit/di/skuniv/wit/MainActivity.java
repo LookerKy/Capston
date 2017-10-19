e936e0400ff8c9f7411f153a035c2243cf66ade0
KyuYoung <nagaraz07@gmail.com>
2017-10-19 17:08:26 +0900

    Start Android

---
+++ app/src/main/java/wit/di/skuniv/wit/MainActivity.java
@@ -0,0 +1,13 @@
+package wit.di.skuniv.wit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
